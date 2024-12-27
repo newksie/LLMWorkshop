3b5955a4b2cd3a773ba7311246c304f72ceef276
Oliver Newcombe <revilo678@gmail.com>
2024-12-27 14:10:38 +0000

    Revert "app fixes"
    This reverts commit 10212adbe2dc4e392d6c0f74fb89d44cfebe404d.

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
-# from app import create_app, socketio
-
-# app = create_app()
-
-# if __name__ == "__main__":
-#     socketio.run(app, debug=True)
-
-from app import create_app, socketio
+from app import app, socketio
 import eventlet
 eventlet.monkey_patch()
-
-app = create_app()
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
