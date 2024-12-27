10212adbe2dc4e392d6c0f74fb89d44cfebe404d
Oliver Newcombe <revilo678@gmail.com>
2024-12-27 13:55:07 +0000

    app fixes

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from app import app, socketio
+# from app import create_app, socketio
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     socketio.run(app, debug=True)
+
+from app import create_app, socketio
 import eventlet
 eventlet.monkey_patch()
+
+app = create_app()
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
