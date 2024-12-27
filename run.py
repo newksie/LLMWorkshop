230f77d88abf841bc6480f336fbc45fde52522bd
Oliver Newcombe <revilo678@gmail.com>
2024-12-27 13:49:02 +0000

    Networking changes

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from app import create_app, socketio
-
-app = create_app()
+from app import app, socketio
+import eventlet
+eventlet.monkey_patch()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000)
